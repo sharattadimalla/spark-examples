1f441e56aadda861c5be277922aff66a102775e6
Sharat C Tadimalla <sharat.tadimalla@gmail.com>
2023-08-18 14:57:32 -0400

    updated spark session to include flexible config

--- pyspark/session/spark_session.py
+++ pyspark/session/spark_session.py
@@ -1,14 +1,23 @@
 from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
 
 
-def get_session():
+def get_session(config: dict) -> SparkSession:
+    """Setup spark session
+
+    Returns:
+        SparkSession: Returns a spark session object
     """
-    Returns a spark session object
-    """
-    # create a spark session
+
+    spark_conf = SparkConf().setAppName("PySpark Application").setMaster("local[*]")
+
+    if len(config.keys()) > 0:
+        for key, value in config.items():
+            spark_conf.set(key, value)
+            print("Set Spark config {0} >> {1}".format(key, value))
+
     spark = (
-        SparkSession.builder.appName("PySpark Application")
-        .master("local[*]")
-        .getOrCreate()
+        SparkSession.builder.config(conf=spark_conf).enableHiveSupport().getOrCreate()
     )
+
     return spark
